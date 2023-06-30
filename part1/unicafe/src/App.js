b391a16758e0f76d2557e0fb18c2b615524da933
SvetlanaMarchenko <semusv@mail.ru>
2023-06-30 09:59:52 +0300

    Finnished 1.6: unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,9 +6,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {setGood(good + 1)}
+  const handleNeutralClick = () => {setNeutral(neutral + 1)}
+  const handleBadClick = () => {setBad(bad + 1)}
+
   return (
     <div>
-      code here
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <button onClick={handleGoodClick}> good </button>
+      <button onClick={handleNeutralClick}> neutral </button>
+      <button onClick={handleBadClick}> bad </button>
     </div>
   )
 }
