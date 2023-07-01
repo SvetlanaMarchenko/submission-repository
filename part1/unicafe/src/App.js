74d054209ab4b817473ce73765ebf26baa1c62ef
SvetlanaMarchenko <semusv@mail.ru>
2023-07-01 11:27:42 +0300

    Finished 1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -5,19 +5,27 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const handleGoodClick = () => {setGood(good + 1)}
-  const handleNeutralClick = () => {setNeutral(neutral + 1)}
-  const handleBadClick = () => {setBad(bad + 1)}
+  const handleGoodClick = () => {setGood(good + 1); setTotal(total + 1)};
+  const handleNeutralClick = () => {setNeutral(neutral + 1); setTotal(total + 1)}
+  const handleBadClick = () => {setBad(bad + 1); setTotal(total + 1)}
+  
 
   return (
     <div>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <h1>give feedback</h1>
       <button onClick={handleGoodClick}> good </button>
       <button onClick={handleNeutralClick}> neutral </button>
       <button onClick={handleBadClick}> bad </button>
+
+      <h1>statistics</h1>
+      <p style={{ margin: 0 }}>Good {good}</p>
+      <p style={{ margin: 0 }}>Neutral {neutral}</p>
+      <p style={{ margin: 0 }}> Bad {bad}</p>
+      <p style={{ margin: 0 }}>all {total}</p>
+      <p style={{ margin: 0 }}>average {total === 0 ? 0 : (good - bad) / total}</p>
+      <p style={{ margin: 0 }}>positive {total === 0 ? 0 : (good * 100) / total} %</p>
     </div>
   )
 }
