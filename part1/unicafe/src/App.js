e0e27d5930bfed28686e2d16df55cdbeaf2ca6b7
SvetlanaMarchenko <semusv@mail.ru>
2023-07-02 10:27:17 +0300

    Finished 1.10: unicafe step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,17 @@
 import React, { useState } from 'react';
 
+const Button = ({ text, handleClick }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p style={{ margin: 0 }}>
+      {text} {value}
+    </p>
+  );
+};
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const average = total > 0 ? (good - bad) / total : 0;
@@ -8,12 +20,12 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h1>Statistics</h1>
-      <p style={{ margin: 0 }}>Good {good}</p>
-      <p style={{ margin: 0 }}>Neutral {neutral}</p>
-      <p style={{ margin: 0 }}>Bad {bad}</p>
-      <p style={{ margin: 0 }}>Total {total}</p>
-      <p style={{ margin: 0 }}>Average {average}</p>
-      <p style={{ margin: 0 }}>Positive {positivePercentage}%</p>
+      <StatisticLine text="Good" value={good} />
+      <StatisticLine text="Neutral" value={neutral} />
+      <StatisticLine text="Bad" value={bad} />
+      <StatisticLine text="Total" value={total} />
+      <StatisticLine text="Average" value={average} />
+      <StatisticLine text="Positive" value={`${positivePercentage}%`} />
     </div>
   );
 };
@@ -46,9 +58,9 @@ const App = () => {
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={handleGoodClick}>Good</button>
-      <button onClick={handleNeutralClick}>Neutral</button>
-      <button onClick={handleBadClick}>Bad</button>
+      <Button text="Good" handleClick={handleGoodClick} />
+      <Button text="Neutral" handleClick={handleNeutralClick} />
+      <Button text="Bad" handleClick={handleBadClick} />
 
       {feedbackGiven ? (
         <Statistics good={good} neutral={neutral} bad={bad} />
@@ -60,43 +72,3 @@ const App = () => {
 };
 
 export default App;
-
-
-// import React, { useState } from 'react';
-// const App = () => {
-//   // save clicks of each button to its own state
-//   const [good, setGood] = useState(0)
-//   const [neutral, setNeutral] = useState(0)
-//   const [bad, setBad] = useState(0)
-//   const [total, setTotal] = useState(0)
-
-//   const handleGoodClick = () => {setGood(good + 1); setTotal(total + 1)};
-//   const handleNeutralClick = () => {setNeutral(neutral + 1); setTotal(total + 1)}
-//   const handleBadClick = () => {setBad(bad + 1); setTotal(total + 1)}
-
-// const Statistics = ({ good, neutral, bad }) => {
-//   const total = good + neutral + bad;
-//   const average = total > 0 ? (good - bad) / total : 0;
-//   const positivePercentage = total > 0 ? (good / total) * 100 : 0}
-
-
-//   return (
-//     <div>
-//       <h1>give feedback</h1>
-//       <button onClick={handleGoodClick}> good </button>
-//       <button onClick={handleNeutralClick}> neutral </button>
-//       <button onClick={handleBadClick}> bad </button>
-//       <Statistics good={good} neutral={neutral} bad={bad} />
-
-//       <h1>statistics</h1>
-//       <p style={{ margin: 0 }}>Good {good}</p>
-//       <p style={{ margin: 0 }}>Neutral {neutral}</p>
-//       <p style={{ margin: 0 }}> Bad {bad}</p>
-//       <p style={{ margin: 0 }}>all {total}</p>
-//       <p style={{ margin: 0 }}>average {average}</p>
-//       <p style={{ margin: 0 }}>positive {positivePercentage} %</p>
-//     </div>
-//   )
-// }
-
-// export default App
