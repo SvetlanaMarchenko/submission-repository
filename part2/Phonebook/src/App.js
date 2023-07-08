0d5cd5df033f3c65d9af9434a6ad72c7d5284966
SvetlanaMarchenko <semusv@mail.ru>
2023-07-08 18:16:24 +0300

    Finished 2.9*: The Phonebook Step4

--- part2/Phonebook/src/App.js
+++ part2/Phonebook/src/App.js
@@ -2,12 +2,19 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number: '056756774'}
-  ]); 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  
   const [newPerson, setNewPerson] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [searchName, setSearchName] = useState('');
 
-  const personsToShow = persons;
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(searchName.toLowerCase())
+  );
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -31,6 +38,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Search: <input value={searchName} onChange={(event) => setSearchName(event.target.value)} />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newPerson} onChange={(event) => setNewPerson(event.target.value)} />
