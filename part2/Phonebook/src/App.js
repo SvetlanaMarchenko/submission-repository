fc1b1d3dfd1b7a36503944fe2edd339ca93fd43e
SvetlanaMarchenko <semusv@mail.ru>
2023-07-09 17:06:44 +0300

    Finished 2.10: The Phonebook Step5

--- part2/Phonebook/src/App.js
+++ part2/Phonebook/src/App.js
@@ -1,62 +1,86 @@
 import { useState } from 'react';
 
+const Filter = ({ searchName, setSearchName }) => {
+  return (
+    <div>
+      Search: <input value={searchName} onChange={(event) => setSearchName(event.target.value)} />
+    </div>
+  );
+};
+
+const PersonInfo = ({ addPerson, newName, setNewName, newNumber, setNewNumber }) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = ({ persons }) => {
+  return (
+    <ul>
+      {persons.map(person => (
+        <li key={person.name}>
+          {person.name} {person.number}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  
-  const [newPerson, setNewPerson] = useState('');
-  const [newNumber, setNewNumber] = useState('');
+  ]);
   const [searchName, setSearchName] = useState('');
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const personsToShow = persons.filter(person =>
     person.name.toLowerCase().includes(searchName.toLowerCase())
   );
 
-  const addPerson = (event) => {
+  const addPerson = event => {
     event.preventDefault();
-    
-    const nameExists = persons.some(person => person.name === newPerson);
-  
-    if (nameExists) {
-      alert(`${newPerson} is already added to the phonebook`);
-    } else {
-      const newPersonObject = {
-        name: newPerson,
-        number: newNumber,
-        important: Math.random() < 0.5, 
-      };
-      setPersons(persons.concat(newPersonObject));
-      setNewPerson('');
-      setNewNumber('');
-    }
+    const newPerson = {
+      name: newName,
+      number: newNumber
+    };
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+    setNewNumber('');
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        Search: <input value={searchName} onChange={(event) => setSearchName(event.target.value)} />
-      </div>
-      <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newPerson} onChange={(event) => setNewPerson(event.target.value)} />
-          <div>number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)} /></div>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {personsToShow.map(person =>
-          <li key={person.name}>{person.name} {person.number}</li>
-        )}
-      </ul>
+
+      <Filter searchName={searchName} setSearchName={setSearchName} />
+
+      <h3>Add a new</h3>
+
+      <PersonInfo
+        addPerson={addPerson}
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons persons={personsToShow} />
     </div>
   );
 };
