0d9fce69ca3d2d7e522598a815c190769031fd9b
SvetlanaMarchenko <semusv@mail.ru>
2023-07-10 13:58:28 +0300

    Finished 2.11: The Phonebook Step6

--- part2/Phonebook/src/App.js
+++ part2/Phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const Filter = ({ searchName, setSearchName }) => {
   return (
@@ -25,6 +26,10 @@ const PersonInfo = ({ addPerson, newName, setNewName, newNumber, setNewNumber })
 };
 
 const Persons = ({ persons }) => {
+  if (persons.length === 0) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <ul>
       {persons.map(person => (
@@ -36,16 +41,25 @@ const Persons = ({ persons }) => {
   );
 };
 
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+  
   const [searchName, setSearchName] = useState('');
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+
+
 
   const personsToShow = persons.filter(person =>
     person.name.toLowerCase().includes(searchName.toLowerCase())
@@ -61,6 +75,8 @@ const App = () => {
     setNewName('');
     setNewNumber('');
   };
+
+  
 
   return (
     <div>
