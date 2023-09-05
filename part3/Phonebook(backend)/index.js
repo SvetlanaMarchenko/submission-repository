5c9bd19cca4ddbef42f2310fb3a3284c60d7b924
SvetlanaMarchenko <semusv@mail.ru>
2023-09-05 12:19:49 +0300

    3.6: Phonebook backend step6

--- part3/Phonebook(backend)/index.js
+++ part3/Phonebook(backend)/index.js
@@ -4,26 +4,26 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/info', (req, res) => {
@@ -49,6 +49,12 @@ app.post('/api/persons', (request, response) => {
       error: 'content missing' 
     })
   }
+
+  // Проверка уникальности имени
+  if (persons.some(existingPerson => existingPerson.name === body.name)) {
+    return response.status(400).json({ error: 'Name must be unique' })
+  }
+
   const person = {
     id: generateId(),
     name: body.name,
@@ -67,9 +73,8 @@ app.get('/api/persons/:id', (request, response) => {
   if (person) {
     response.json(person)
   } else {
-    response.status(404).end()
+    response.status(404).json({ error: 'Person not found' })
   }
-
 })
 
 app.delete('/api/persons/:id', (request, response) => {
