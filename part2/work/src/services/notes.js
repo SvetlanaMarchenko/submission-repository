bdfbab90c55612aaef872f7f7f957d87156e5bb8
SvetlanaMarchenko <semusv@mail.ru>
2023-09-06 23:17:40 +0300

    working tree

--- part2/work/src/services/notes.js
+++ part2/work/src/services/notes.js
@@ -1,21 +1,14 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = '/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  return request.then(response => 
-    response.data
-    )
+  return request.then(response => response.data)
 }
 
 const create = newObject => {
   const request = axios.post(baseUrl, newObject)
-  return request.then(
-    response => {
-    console.log(response)
-    return response.data
-    }
-    )
+  return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
@@ -24,7 +17,5 @@ const update = (id, newObject) => {
 }
 
 export default { 
-  getAll, 
-  create, 
-  update 
+  getAll, create, update 
 }
