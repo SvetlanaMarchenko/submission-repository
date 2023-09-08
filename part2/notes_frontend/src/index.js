fe4524c6e43735456273948dbb5485c7edbb7fc5
SvetlanaMarchenko <semusv@mail.ru>
2023-09-08 10:45:53 +0300

    Renamed_phonebook_folders

--- part2/notes_frontend/src/index.js
+++ part2/notes_frontend/src/index.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 
 import App from './App'
 
-// axios.get('http://localhost:3001/api/notes').then(response => {
-axios.get('/api/notes').then(response => {
+axios.get('http://localhost:3001/api/notes').then(response => {
+// axios.get('/api/notes').then(response => {
   const notes = response.data
   ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
 })
