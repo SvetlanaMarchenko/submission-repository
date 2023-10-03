f17b57ca3b52abdec879951c4ae80d557bab60f5
SvetlanaMarchenko <semusv@mail.ru>
2023-10-03 12:31:08 +0300

    Creating a new note with user administration

--- part4/notes_backend/utils/for_testing.js
+++ part4/notes_backend/utils/for_testing.js
@@ -5,11 +5,10 @@ const reverse = (string) => {
     .join('')
 }
 
-const average = array => {
+const average = (array) => {
   const reducer = (sum, item) => {
     return sum + item
   }
-
   return array.length === 0
     ? 0
     : array.reduce(reducer, 0) / array.length
