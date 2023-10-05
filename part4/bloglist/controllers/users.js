ffe7ba3c66765d5125bf6b8f581bbbf0bcd7d0f1
SvetlanaMarchenko <semusv@mail.ru>
2023-10-05 13:58:01 +0300

    4.15: bloglist expansion, step3 - DONE

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -7,6 +7,12 @@ usersRouter.get('/', async (request, response) => {
   response.json(users)
 })
 
+// usersRouter.get('/', async (request, response) => {
+//   const users = await User
+//     .find({}).populate('blogs', { username: 1, name: 1 })
+//   response.json(users)
+// })
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
