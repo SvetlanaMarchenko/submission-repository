b922ccb96ba420d76c5a8b363b3f871fc16fe3c7
SvetlanaMarchenko <semusv@mail.ru>
2023-10-12 14:45:30 +0300

    Created child

--- part5/notes_frontend/src/services/notes.js
+++ part5/notes_frontend/src/services/notes.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/notes'
-// const baseUrl = '/api/notes'
 
 let token = null
 
@@ -30,3 +29,6 @@ const update = (id, newObject) => {
 export default { 
   getAll, create, update, setToken
 }
+
+
+
