a6333a1929310b4d14e955ac68360565f89550db
SvetlanaMarchenko <semusv@mail.ru>
2023-12-14 13:22:10 +0200

    About - DONE

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -46,19 +46,19 @@ const AnecdoteList = ({ anecdotes }) => (
 
 
 
-// const About = () => (
-//   <div>
-//     <h2>About anecdote app</h2>
-//     <p>According to Wikipedia:</p>
+const About = () => (
+  <div>
+    <h2>About anecdote app</h2>
+    <p>According to Wikipedia:</p>
 
-//     <em>An anecdote is a brief, revealing account of an individual person or an incident.
-//       Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-//       such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-//       An anecdote is "a story with a point."</em>
+    <em>An anecdote is a brief, revealing account of an individual person or an incident.
+      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+      An anecdote is "a story with a point."</em>
 
-//     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
-//   </div>
-// )
+    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+  </div>
+)
 
 
 // const CreateNew = (props) => {
@@ -152,14 +152,14 @@ const App = () => {
           <Link style={padding} to="/">Menu</Link>
           <Link style={padding} to="/anecdotes">anecdotes</Link>
           {/* <Link style={padding} to="/createNew">create new</Link> */}
-          {/* <Link style={padding} to="/about">about</Link> */}
+          <Link style={padding} to="/about">about</Link>
         </div>
       
         <Routes>
           <Route path="/anecdotes/:id" element={<Anecdote anecdotes={anecdotes} />} />
           <Route path="/anecdotes" element={<AnecdoteList anecdotes={anecdotes} />} />
           {/* <Route path="/createNew" element={<CreateNew addNew={addNew} />} /> */}
-          {/* <Route path="/about" element={About /} /> */}
+          <Route path="/about" element= {<About />} />
           <Route path="/" element={<Menu />} />
         </Routes>
       </Router>
