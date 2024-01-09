f53c74473d45ba508e508de25c4f46b12e50ec32
SvetlanaMarchenko <semusv@mail.ru>
2024-01-09 14:11:20 +0200

    SOme new changes

--- part7/Page/src/index.js
+++ part7/Page/src/index.js
@@ -1,6 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 
-import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';  
+import App from './App';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <Router>
+//       <App />
+//     </Router>
+//   );
+
+
+
+
+  
+  
+  
